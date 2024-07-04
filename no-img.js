5567218f2727c3e5cb3945d65aa70cad15bb8a41
clientsnowtech <hello@clientsnow.in>
2024-07-05 02:26:12 +0530

    Create no-img.js

---
+++ no-img.js
@@ -0,0 +1,1 @@
+function handleImageError(){this.onerror=null,this.src="https://www.clientsnow.in/data/no-img.jpg"}const allImages=document.querySelectorAll("img");allImages.forEach(r=>{r.onerror=handleImageError});
